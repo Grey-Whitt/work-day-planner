a9bafc191ebf76d3d3bd1ddffc96f2a310fe59a8
greyw <greywhitt@gmail.com>
2020-08-28 18:31:06 -0500

    tasks now save and load

--- Develop/script.js
+++ Develop/script.js
@@ -39,7 +39,55 @@ $('.container .locked').on('click', function() {
 
 });
 
+
+
 var loadTasks = function() {
+    var nineContent =  localStorage.getItem('09')
+    var tenContent =  localStorage.getItem('010')
+    var elevenContent =  localStorage.getItem('011')
+    var twelveContent =  localStorage.getItem('012')
+    var thirteenContent =  localStorage.getItem('013')
+    var fourteenContent =  localStorage.getItem('014')
+    var fifteenContent =  localStorage.getItem('015')
+    var sixteenContent =  localStorage.getItem('016')
+    var seventeenContent =  localStorage.getItem('017')
+
+
+    if (nineContent != null) {
+        $('#txt-09').val(nineContent)
+    }
+
+    if (tenContent != null) {
+        $('#txt-010').val(tenContent)
+    }
+
+    if (elevenContent != null) {
+        $('#txt-011').val(elevenContent)
+    }
+
+    if (twelveContent != null) {
+        $('#txt-012').val(twelveContent)
+    }
+
+    if (thirteenContent != null) {
+        $('#txt-013').val(thirteenContent)
+    }
+
+    if (fourteenContent != null) {
+        $('#txt-014').val(fourteenContent)
+    }
+
+    if (fifteenContent != null) {
+        $('#txt-015').val(fifteenContent)
+    }
+
+    if (sixteenContent != null) {
+        $('#txt-016').val(sixteenContent)
+    }
+
+    if (seventeenContent != null) {
+        $('#txt-017').val(seventeenContent)
+    }
 
 }
 
@@ -52,6 +100,8 @@ setInterval(todayDate, 1000*60);
 //Audits t
 auditTimes();
 setInterval(auditTimes, 1000*60);
+
+loadTasks();
 
 
 
